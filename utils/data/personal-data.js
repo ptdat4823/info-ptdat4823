40e7b95889dc70d633c6341043a4cef47618f5a5
ptdat4823 <phamtiendat4823@gmail.com>
2025-03-31 11:15:16 +0700

    change resume url

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -12,5 +12,5 @@ export const personalData = {
   linkedIn: "https://www.linkedin.com/in/dat-pham-526299252/",
   leetcode: "https://leetcode.com/u/ptdat4823/",
   devUsername: "ptdat4823",
-  cv: "https://drive.google.com/file/d/1Gc4NsczZQq3YkVXLYzeR6MK9uJ4UR09i/view?usp=sharing",
+  cv: "https://drive.google.com/file/d/1QX4jIezt9gqMrNGvWzZFCS_y3CdNd43C/view?usp=sharing",
 };
